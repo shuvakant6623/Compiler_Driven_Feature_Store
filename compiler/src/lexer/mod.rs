1de48aafc5aac4c580e269e816a5506037d9a029
shuvakant6623 <scientefic2612@gmail.com>
2026-04-03 23:58:41 +0530

    Lexer Partial logic built

--- compiler/src/lexer/mod.rs
+++ compiler/src/lexer/mod.rs
@@ -37,11 +37,45 @@ impl Lexer {
             position: 0,
             line: 1,
             tokens: Vec::new(),
-            peek_position: 1,
+            if input.len() > 1 {
+                peek_position: 1
+            } else { 
+                peek_position: 0,
+            }
         }
     }
 
     fn tokenize(&mut self) {
-        
+        while self.position < self.input.len() {
+            let current_char = self.input.chars().nth(self.position).unwrap();
+            if current_cahr == '\n' {
+                self.line += 1;
+                self.position += 1;;
+            } else if current_char.is_whitespace() {
+                self.position += 1;
+            } else if current_char.isalphabetic() {
+                let identifier = self.read_identifier();
+                let token_type = self.lookup_keyword(&identifier);
+                self.tokens.push(Token {
+                    token_type,
+                    value: identifier,
+                    line: self.line,
+                });
+            } else if current_char.is_digit(10) {
+                let number = self.read_number();
+                self.tokens.push(Token {
+                    token_type: TokenType::Integer(number),
+                    value: number.to_string(),
+                    line: self.line,
+                });
+            } else {
+                self.tokens.push(Token {
+                    token_type: TokenType::Error(format!("Unexpected character: {}", current_char)),
+                    value: current_char.to_string(),
+                    line: self.line,
+                });
+                self.position += 1;
+            }
+        }
     }
 }
