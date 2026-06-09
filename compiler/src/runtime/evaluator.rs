6f81fb9469dd0ced78cab88d53f2781b4185a291
shuvakant patra <scientefic2612@gmail.com>
2026-06-10 00:47:46 +0530

    Refactor: Add evaluator with variable handling(get ,set, assign)

--- compiler/src/runtime/evaluator.rs
+++ compiler/src/runtime/evaluator.rs
@@ -1,7 +1,7 @@
 use crate::runtime::value::Value;
 use crate::runtime::environment::Environment;
 use crate::runtime::error::RuntimeError;
-use crate::parser::ast::Value as AstValue;
+use crate::parser::ast::{Value as AstValue, Stmt};
 
 pub struct Evaluator {
     pub env: Environment,
@@ -14,16 +14,21 @@ impl Evaluator {
         }
     }
 
-    pub fn eval_value(&self, value: &AstValue) -> Result<Value, RuntimeError> {
+    pub fn eval_value(&mut self, value: &AstValue) -> Result<Value, RuntimeError> {
         match value {
             AstValue::Integer(i) => Ok(Value::Int(*i)),
-            AstValue::Float(f) => Ok(Value::Int(*f as i64)), // simple for now
+
+            AstValue::Float(_) => Err(RuntimeError::TypeMismatch(
+                "Float not supported yet".to_string(),
+            )),
+
             AstValue::Text(s) => Ok(Value::String(s.clone())),
+
             AstValue::Identifier(name) => {
-                match self.env.get(name) {
-                    Some(v) => Ok(v.clone()),
-                    None => Err(RuntimeError::UndefinedVariable(name.clone())),
-                }
+                self.env
+                    .get(name)
+                    .cloned()
+                    .ok_or(RuntimeError::UndefinedVariable(name.clone()))
             }
         }
     }
@@ -35,41 +40,65 @@ impl Evaluator {
         right: Value,
     ) -> Result<Value, RuntimeError> {
         match (left, right) {
-            (Value::Int(a), Value::Int(b)) => {
-                match op {
-                    "+" => Ok(Value::Int(a + b)),
-                    "-" => Ok(Value::Int(a - b)),
-                    "*" => Ok(Value::Int(a * b)),
-                    "/" => {
-                        if b == 0 {
-                            Err(RuntimeError::DivisionByZero)
-                        } else {
-                            Ok(Value::Int(a / b))
-                        }
+            (Value::Int(a), Value::Int(b)) => match op {
+                "+" => Ok(Value::Int(a + b)),
+                "-" => Ok(Value::Int(a - b)),
+                "*" => Ok(Value::Int(a * b)),
+                "/" => {
+                    if b == 0 {
+                        Err(RuntimeError::DivisionByZero)
+                    } else {
+                        Ok(Value::Int(a / b))
                     }
-                    "==" => Ok(Value::Bool(a == b)),
-                    "!=" => Ok(Value::Bool(a != b)),
-                    ">" => Ok(Value::Bool(a > b)),
-                    "<" => Ok(Value::Bool(a < b)),
-                    _ => Err(RuntimeError::TypeMismatch(format!(
-                        "Unsupported operator '{}'", op
-                    ))),
                 }
-            }
+                "==" => Ok(Value::Bool(a == b)),
+                "!=" => Ok(Value::Bool(a != b)),
+                ">" => Ok(Value::Bool(a > b)),
+                "<" => Ok(Value::Bool(a < b)),
 
-            (Value::String(a), Value::String(b)) => {
-                match op {
-                    "==" => Ok(Value::Bool(a == b)),
-                    "!=" => Ok(Value::Bool(a != b)),
-                    _ => Err(RuntimeError::TypeMismatch(format!(
-                        "Invalid string operation '{}'", op
-                    ))),
-                }
-            }
+                _ => Err(RuntimeError::TypeMismatch(format!(
+                    "Unsupported operator '{}'",
+                    op
+                ))),
+            },
+
+            (Value::String(a), Value::String(b)) => match op {
+                "==" => Ok(Value::Bool(a == b)),
+                "!=" => Ok(Value::Bool(a != b)),
+
+                _ => Err(RuntimeError::TypeMismatch(format!(
+                    "Invalid string operation '{}'",
+                    op
+                ))),
+            },
 
             _ => Err(RuntimeError::TypeMismatch(
                 "Mismatched types in binary operation".to_string(),
             )),
+        }
+    }
+
+    pub fn eval_stmt(&mut self, stmt: Stmt) -> Result<(), RuntimeError> {
+        match stmt {
+            // let x = value;
+            Stmt::Let(name, expr) => {
+                let value = self.eval_value(&expr)?;
+                self.env.set(name, value);   
+                Ok(())
+            }
+
+            // x = value;
+            Stmt::Assign(name, expr) => {
+                let value = self.eval_value(&expr)?;
+                self.env.assign(name, value)?; 
+                Ok(())
+            }
+
+            // expression statement (like: x + 5)
+            Stmt::Expr(expr) => {
+                self.eval_value(&expr)?;  // just evaluate
+                Ok(())
+            }
         }
     }
 }
